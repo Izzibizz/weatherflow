ed3253b49b7f520a8876554d8a95c5447922f44a
izzibizz <contact@izabellind.com>
2024-09-17 10:09:17 +0200

    added cloudinary

--- src/components/TodaysForecast.jsx
+++ src/components/TodaysForecast.jsx
@@ -10,28 +10,27 @@ export const TodaysForecast = () => {
   const weatherNow = forecastData?.[0]?.weather?.[0]?.description
   const temperatureNow = forecastData?.[0]?.main?.temp?.toFixed(1)
   
- /*  const videoByWeather = () => {
+  const videoByWeather = () => {
     if (weatherNow) {
-      if (weatherNow === "few clouds") return "/little-clouds.mp4";
-      else if (weatherNow === "scattered clouds") return "/scattered-clouds.mp4";
-      else if (weatherNow?.includes("clouds")) return "/broken-clouds.mov"; // Optional chaining
-      else if (weatherNow?.includes("clear")) return "/clear.mp4";
-      else if (weatherNow?.includes("rain")) return "/rain.mov";
-      else if (weatherNow?.includes("thunderstorm")) return "/rain-thunder.mov";
-      else return "/broken-clouds.mov";
+      if (weatherNow === "few clouds") return "https://res.cloudinary.com/dbf8xygxz/video/upload/v1726146428/little-clouds_ne5eaw.mp4";
+      else if (weatherNow === "scattered clouds") return "https://res.cloudinary.com/dbf8xygxz/video/upload/v1726146429/scattered-clouds_hymr9l.mp4";
+      else if (weatherNow?.includes("clouds")) return "https://res.cloudinary.com/dbf8xygxz/video/upload/v1726146427/broken-clouds_yabiso.mp4"; 
+      else if (weatherNow?.includes("clear")) return "https://res.cloudinary.com/dbf8xygxz/video/upload/v1726146442/clear_bpyvlj.mp4";
+      else if (weatherNow?.includes("thunderstorm")) return "https://res.cloudinary.com/dbf8xygxz/video/upload/v1726146452/rain-thunder_pwnr4q.mov";
+      else if (weatherNow?.includes("rain")) return "https://res.cloudinary.com/dbf8xygxz/video/upload/v1726146440/rain_wgjndm.mov";
     }
-    return "/default-video.mp4"; // Fallback in case weatherNow is not available
+    return "/clear.mp4"; // Fallback in case weatherNow is not available
   };
   
   const iconByWeather = () => {
     if (weatherNow) {
-      if (weatherNow.includes("clear")) return "/sun2.svg";
+      if (weatherNow.includes("clear")) return "/sun.svg";
       else if (weatherNow.includes("clouds")) return "/clouds.svg";
       else if (weatherNow.includes("rain")) return "/rain.svg";
       else if (weatherNow.includes("thunderstorm")) return "/thunder.svg";
     }
     return "/clouds.svg"; // Default fallback
-  }; */
+  };
   
   const handleArrowClick = () => {
     setExtentionIsVisible(!extentionIsVisible)
@@ -43,7 +42,7 @@ export const TodaysForecast = () => {
      autoPlay
      muted
      loop
-     src="clear.mp4" />
+     src={videoByWeather()} />
 <div className="absolute h-[200px] z-30 top-[100px] tablet:top-1/3 p-8 flex flex-col w-full">
       <h2 className="text-2xl flex font-h ">{temperatureNow}<span className="text-sm ">°C</span></h2>
       <h3>{weatherNow}</h3>
@@ -67,7 +66,7 @@ export const TodaysForecast = () => {
     } w-8 h-8 text-white absolute z-50 fadeIn`}
   />
   </div>
-  <img src="sun.svg" alt={weatherNow} className="w-20 h-20 ml-auto "/>
+  <img src={iconByWeather()} alt={weatherNow} className="w-20 h-20 ml-auto "/>
       <div className={`${extentionIsVisible ? "flex" : "hidden"} fadeIn mt-40 `}>
 hello
 </div>
