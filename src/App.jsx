0a0dc926279215555628135481faf0c4c7b9c022
izzibizz <contact@izabellind.com>
2024-09-30 19:26:57 +0200

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Header } from "./components/Header"
 const App = () => {
   return (
     <div className=" w-screen min-h-screen max-w-screen h-screen bg-gradient-to-r from-[#0c1647] to-[#397495] flex flex-col justify-center items-center">
-      <img src="/weatherflow-white.svg" alt="logo" className="absolute top-10 tablet:top-6 right-2 tablet:right-6 tablet:w-1/3 z-30 hidden tablet:block"/>
-      <h1 className="font-heading text-white absolute top-20 right-10 hidden tablet:block">We Follow the Flow of the weather</h1>
+      <img src="/weatherflow-1.png" alt="logo" className="absolute top-10 tablet:top-6 right-2 tablet:right-6 tablet:w-1/3 z-30 hidden tablet:block"/>
+      <h1 className="font-heading text-white absolute top-32 right-10 hidden tablet:block">We Follow the Flow of the weather</h1>
       <div className="w-full h-full bg-white tablet:h-[768px] tablet:w-[376px] tablet:mt-8 tablet:bg-iphone bg-cover bg-center flex flex-col tablet:p-[17px] tablet:rounded-[60px]">
         <div className="w-full h-full relative">
         <Header/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
     const { showSearchField } = useForecastStore()
   return (
     <header className={`h-16 absolute mt-10 w-full  z-50 flex items-center  ${showSearchField ? "justify-end" : "justify-between"} px-10 transition-all duration-500`}>
-         <img src="/weatherflow-white.svg" alt="logo" className={`w-[140px] ${showSearchField ? "hidden " : "block animate-fadeIn"}`}/>
+         <img src="/weatherflow-1.png" alt="logo" className={`w-[140px] ${showSearchField ? "hidden " : "block animate-fadeIn"}`}/>
          <div className={` gap-2 ${showSearchField && "w-full justify-end"} flex`}>
             < Searchbar />
             < GetLocation />
